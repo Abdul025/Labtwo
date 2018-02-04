a18ada5c2a8417c64ceaf4797f7ab10561eb8661
Abdul025 <ghafor_n@live.com>
2018-02-03 21:16:50 -0500

    Initial commit

---
+++ LabTwo/Assets/script/ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+    public Sprite myPlayer;
+    public Rigidbody2D image;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        SpriteRenderer sRend = image.GetComponent<SpriteRenderer>();
+        sRend.sprite = myPlayer;
+    }
+}
